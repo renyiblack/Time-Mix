85cbc0597accbd843fbc5211fa1534bd9970908b
João Anisio Marinho da Nobrega <joaoanisiomn@hotmail.com>
2022-07-06 18:04:43 -0300

    integracao combate

---
+++ Turn Based Training/Scripts/State.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var current_health = 50.0
+var max_health = 50.0
+var damage = 15.0
+var hit_rate = 95.0
+var def_rate = 1.0
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
