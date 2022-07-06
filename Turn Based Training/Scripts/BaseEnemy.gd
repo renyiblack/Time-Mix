79c992737e60899f51af3e63e3464c15b4d988f1
João Anisio Marinho da Nobrega <85528178+CaduceusJ@users.noreply.github.com>
2022-07-06 17:41:42 -0300

    Deletando para realizar um Update

--- Turn Based Training/Scripts/BaseEnemy.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-
-export(String) var name = "Enemy"
-export(Texture) var texture = null
-export(int) var health = 50.0
-export(int) var damage = 20.0
-export(int) var hit_rate = 80.0
